d7766f68230c4d93b3e8153f7b4a905be663fc05
davscharrer <david.scharrer@school4games.net>
2014-06-02 00:36:17 +0200

    Kleinigkeiten
    - Trampolin added
    - Collision Manager added and done
    - Riesenrad added

---
+++ RoundAboutMidnight/Assets/Scenes/Spielzeugladen/Scripts/Manager/Collision_Manager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision_Manager : MonoBehaviour {
+
+    public GameObject moveObjectCubeBall2;
+    public GameObject moveObjectCubeBall3;
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.name == "Trampolin")
+        {
+            CharacterSwitchManager.Instance.currentPlayer.playerRigidbody.velocity = new Vector3(CharacterSwitchManager.Instance.currentPlayer.playerRigidbody.velocity.x, 10);
+        }
+
+        if (other.gameObject.name == "moveObjectBall2")
+        {
+            if (CharacterSwitchManager.Instance.currentPlayer.name == "Ball 2")
+            {
+                moveObjectCubeBall2.transform.rigidbody.mass = 0.1f;
+                Debug.Log("1");
+            }
+        }
+
+        if (other.gameObject.name == "moveObjectBall3")
+        {
+            if (CharacterSwitchManager.Instance.currentPlayer.name == "Ball 3")
+            {
+                moveObjectCubeBall3.transform.rigidbody.mass = 0.1f;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (CharacterSwitchManager.Instance.currentPlayer.name == "Ball 1" && moveObjectCubeBall2.transform.rigidbody.mass < 500f)
+        {
+            moveObjectCubeBall2.transform.rigidbody.mass = 500f;
+        }
+        if (CharacterSwitchManager.Instance.currentPlayer.name != "Ball 3" && moveObjectCubeBall2.transform.rigidbody.mass < 500f)
+        {
+            moveObjectCubeBall3.transform.rigidbody.mass = 500f;
+        }
+    }
+}
---
+++ RoundAboutMidnight/Assets/Scenes/Spielzeugladen/Scripts/Manager/EnableBalls.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableBalls : MonoBehaviour {
+
+    public static EnableBalls Instance;
+
+    public bool enableBall1, enableBall2, enableBall3;
+    public Transform _ball3, _ball2, _ball1;
+    public GameObject emotionBall3;
+
+ 
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+}
